24e07d1229d2b295488454d1f145531c180e8e5a
CoderEx24 <coderex24@protonmail.com>
2023-12-13 09:27:01 +0200

    added a simple main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+mod lexer;
+
+use lexer::Lexer;
+use std::env::args;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = args().collect();
+
+    if args.len() > 2 {
+        panic!("[ERROR] too much arguments");
+    }
+
+    let filename = &args[1];
+
+    let mut lexer = Lexer::for_file(filename);
+    lexer.analyse();
+
+    lexer.get_tokens().iter().for_each(|t| println!("{}", t));
 }
