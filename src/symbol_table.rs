72c59279d32c810e60fb32d2c6284d196a3228cd
CoderEx24 <coderex24@protonmail.com>
2024-10-14 20:32:48 +0300

    symbol_table: let's try that again

--- src/symbol_table.rs
+++ src/symbol_table.rs
@@ -1,101 +1,148 @@
+use std::cell::RefCell;
 use std::collections::HashMap;
+use std::rc::Rc;
 
 #[derive(PartialEq, Clone)]
-enum Type<'a> {
+enum Type {
     String,
     Object,
     Bool,
     Int,
     IO,
-    Custom(&'a Class<'a>),
+    Custom(Rc<RefCell<Class>>),
 }
 
 #[derive(PartialEq, Clone)]
-struct Method<'a> {
+struct Method {
     pub name: String,
-    pub classname: String,
-    pub parameters: HashMap<String, Type<'a>>,
+    pub class: Rc<RefCell<Class>>,
+    pub parameters: HashMap<String, Type>,
 }
 
-impl<'a> Method<'a> {
+impl Method {
     pub fn label(&self) -> String {
-        format!("{}_{}", self.classname, self.name)
+        format!("{}_{}", self.class.borrow().name, self.name)
     }
 }
 
 #[derive(PartialEq, Clone)]
-enum ClassFeature<'a> {
-    Member(String, Type<'a>),
-    Method(Method<'a>),
+enum ClassFeature {
+    Member(String, Type),
+    Method(Method),
 }
 
-#[derive(PartialEq, Clone)]
-struct Class<'a> {
-    pub name: String,
-    pub parent: Option<Type<'a>>,
-    pub features: HashMap<String, ClassFeature<'a>>,
-}
-
-impl<'a> Class<'a> {
-    pub fn contains_feature(&self, feature_name: &str) -> bool {
-        self.features.contains_key(feature_name)
-    }
-
-    pub fn add_member_variable(&mut self, name: &str, type_: &'a Type) -> Result<(), String> {
-        if self.contains_feature(name) {
-            Err(format!("{} already contains member variable {}", self.name, name))
-        } else {
-            let new_member_variable = 
-                ClassFeature::Member(name.to_owned(), type_.clone());
-
-            self.features.insert(name.to_owned(), new_member_variable);
-            Ok(())
-        }
-    }
-
-    pub fn add_method(&'a mut self, name: &str, parameters: HashMap<String, Type<'a>>) -> Result<(), String> {
-        if self.contains_feature(name) {
-            Err(format!("{} already contains method {}", self.name, name))
-        } else {
-            let n = name.to_string();
-            let new_method = Method {
-                name: n,
-                classname: self.name.clone(),
-                parameters,
-            };
-
-            let new_method = ClassFeature::Method(new_method);
-
-            self.features.insert(name.to_owned(), new_method);
-            Ok(())
-        }
-    }
-}
-
-impl<'a> Type<'a> {
-    pub fn contains_feature(&self, feature_name: &str) -> bool {
+impl ClassFeature {
+    pub fn is_member(&self) -> bool {
         match self {
-            Type::Object => match feature_name {
-                "abort" | "type_name" | "copy" => true,
-                _ => false,
-            },
-            Type::IO => match feature_name {
-                "out_string" | "out_int" |
-                    "in_string" | "in_int" => true,
-                _ => false,
-            },
-            Type::String => match feature_name {
-                "length" | "concat" | "substr" => true,
-                _ => false,
-            },
-            Type::Custom(t) => t.contains_feature(feature_name),
+            ClassFeature::Member(_, _) => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_method(&self) -> bool {
+        match self {
+            ClassFeature::Method(_) => true,
             _ => false,
         }
     }
 }
 
 #[derive(PartialEq, Clone)]
-struct Expr<'a> {
-    pub type_: Type<'a>,
+struct Class {
     pub name: String,
+    pub parent: Type,
+    pub features: HashMap<String, ClassFeature>,
+}
+
+impl Class {
+    pub fn new(name: &str, parent: Option<Type>) -> Self {
+        Self {
+            name: name.to_owned(),
+            parent: parent.unwrap_or(Type::Object),
+            features: HashMap::new(),
+        }
+    }
+
+    pub fn contains_member_variable(&self, name: &str) -> bool {
+        self.features.get(name).map_or_else(
+            || self.parent.contains_member_variable(name),
+            |u| u.is_member(),
+        )
+    }
+
+    pub fn contains_method(&self, name: &str) -> bool {
+        self.features.get(name).map_or_else(
+            || self.parent.contains_method(name),
+            |u| u.is_method()
+        )
+    }
+
+    pub fn add_member_variable(&mut self, name: &str, type_: &Type) -> Result<(), String> {
+        if self.contains_member_variable(name) {
+            Err(format!(
+                "{} already contains member variable {}",
+                self.name, name
+            ))
+        } else {
+            let new_member_variable = ClassFeature::Member(name.to_owned(), type_.clone());
+
+            self.features.insert(name.to_owned(), new_member_variable);
+            Ok(())
+        }
+    }
+
+    // NOTE: The method needs to be constructed outside the class
+    //       because the Env object is the main owner of all classes
+    pub fn add_method(&mut self, method: Method) -> Result<(), String> {
+        if self.contains_method(method.name.as_str()) {
+            Err(format!(
+                "{} already contains method {}",
+                self.name, method.name
+            ))
+        } else {
+            self.features
+                .insert(method.name.clone(), ClassFeature::Method(method));
+            Ok(())
+        }
+    }
+
+    pub fn redefine_method(&mut self, method: Method) -> Result<(), String> {
+        if !self.contains_method(method.name.as_str()) {
+            Err(format!(
+                "method {} is not defined already", 
+                method.name
+            ))
+        } else {
+            self.features.insert(method.name.clone(), ClassFeature::Method(method));
+            Ok(())
+        }
+    }
+}
+
+impl Type {
+    pub fn contains_member_variable(&self, name: &str) -> bool {
+        match self {
+            Type::Custom(t) => t.borrow().contains_member_variable(name),
+            _ => false,
+        }
+    }
+
+    pub fn contains_method(&self, name: &str) -> bool {
+        match self {
+            Type::Object => match name {
+                "abort" | "type_name" | "copy" => true,
+                _ => false,
+            },
+            Type::IO => match name {
+                "out_string" | "out_int" | "in_string" | "in_int" => true,
+                _ => false,
+            },
+            Type::String => match name {
+                "length" | "concat" | "substr" => true,
+                _ => false,
+            },
+            Type::Custom(t) => t.borrow().contains_method(name),
+            _ => false,
+        }
+    }
 }
